1a7494ef26a26a44fe2331f01d754e7d81bdfa7a
Vasanth Prabhakaran <vasanthprabhakaran1999@gmail.com>
2020-12-06 00:27:55 +0530

    Conditional Content And Lists Completed

---
+++ Basics/Conditional Content & Lists/app.js
@@ -0,0 +1,20 @@
+// @ts-ignore
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredGoalValue: '',
+      goals: []
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  },
+});
+
+app.mount('#user-goals');
