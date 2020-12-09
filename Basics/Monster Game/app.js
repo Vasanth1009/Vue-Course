c15d884d8844d9a8a6b945ac284255f8ff5c8fe4
Vasanth Prabhakaran <vasanthprabhakaran1999@gmail.com>
2020-12-09 19:51:55 +0530

    Monster Game Completed

--- Basics/Monster Game/app.js
+++ Basics/Monster Game/app.js
@@ -5,7 +5,8 @@ const app = Vue.createApp({
             monsterHealth: 100,
             humanHealth: 100,
             attackCount: 0,
-            winner: null
+            winner: null,
+            logMessages: [],
         };
     },
     watch: {
@@ -13,33 +14,62 @@ const app = Vue.createApp({
             // @ts-ignore
             if (value <= 0 && this.monsterHealth <= 0) {
                 this.winner = 'draw';
+                // @ts-ignore
+                this.humanHealth = 0;
+                this.monsterHealth = 0;
             } else if (value <= 0) {
                 this.winner = 'monster';
+                // @ts-ignore
+                this.humanHealth = 0;
             }
         },
         monsterHealth(value) {
             // @ts-ignore
             if (value <= 0 && this.humanHealth <= 0) {
-                this.winner = 'draw';
+              this.winner = 'draw';
+              // @ts-ignore
+              this.humanHealth = 0;
+              this.monsterHealth = 0;
             }
             else if (value <= 0) {
                 this.winner = 'human';
+                this.monsterHealth = 0;
             }
         },
         
     },
     computed: {
         monsterHealthBar() {
-            return { width: this.monsterHealth + '%' };
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
+            else {
+                return { width: this.monsterHealth + '%' };
+            }
         },
         humanHealthBar() {
-            return { width: this.humanHealth + '%' };
+            if (this.humanHealth < 0) {
+                return { width: '0%' };
+            }
+            else {
+                return { width: this.humanHealth + '%' };
+            }
         },
         specialAttackVisible() {
             return this.attackCount % 3 !== 0;
+        },
+        healVisible() {
+            return this.attackCount % 2 !== 0;
         }
     },
     methods: {
+        newGame() {
+            this.monsterHealth = 100;
+            this.humanHealth = 100;
+            this.attackCount = 0;
+            this.winner = null;
+            this.logMessages = [];
+        },
         getRandomValue(min, max) {
             const value = Math.floor(Math.random() * (max - min)) + min;
             return value;
@@ -48,17 +78,20 @@ const app = Vue.createApp({
             this.attackCount++;
             const value = this.getRandomValue(5, 12);
             this.monsterHealth -= value;
+            this.battleLog('You', 'attack', value)
             this.attackHuman();
         },
         attackHuman() {
             const value = this.getRandomValue(8, 15);
             this.humanHealth -= value;
             console.log(this.monsterHealth + ' ' + this.humanHealth);
+            this.battleLog('Monster', 'attack', value);
         },
         specialAttackMonster() {
             this.attackCount++;
             const value = this.getRandomValue(10, 20);
             this.monsterHealth -= value;
+            this.battleLog('You', 'attack', value);
             this.attackHuman();
         },
         healHuman() {
@@ -70,7 +103,18 @@ const app = Vue.createApp({
             else {
                 this.humanHealth += value;
             }
+            this.battleLog('You', 'heal', value);
             this.attackHuman();
+        },
+        surrender() {
+            this.winner = 'monster';
+        },
+        battleLog(player, work, health) {
+            this.logMessages.unshift({
+                actionBy: player,
+                actionType: work,
+                actionValue: health
+            });
         }
     },
 });
