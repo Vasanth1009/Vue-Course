611f5f9664beb94bb1fb8ccedfed5d73bf664c0d
Vasanth <vasanthprabhakaran1999@gmail.com>
2020-12-13 23:20:26 +0530

    Friend-Contact Created

---
+++ Vue-App/vue-course-app/src/Components/FriendContact.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <h2>{{ friend.name }} {{ favorite }}</h2>
+    <button @click="showAndHideDetails">{{ details ? "Hide" : "Show" }} Details</button>
+    <button @click="">{{ favorite ? "Hide" : "Show" }}Favorite</button>
+    <ul v-if="details">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      details: false,
+    };
+  },
+  methods: {
+    showAndHideDetails() {
+      this.details = !this.details;
+    },
+  },
+};
+</script>
--- Vue-App/vue-course-app/src/main.js
+++ Vue-App/vue-course-app/src/main.js
@@ -1,3 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import FriendContact from './Components/FriendContact.vue';
 
-createApp({}).mount('#app')
+const app = createApp(App);
+app.component('friend-contact', FriendContact);
+app.mount('#app');
